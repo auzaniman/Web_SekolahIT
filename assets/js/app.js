d2dde83e73edc6f42a135445dd4cd697cb65692f
Auzan <auzan.ip@gmail.com>
2021-10-09 11:16:34 +0800

    add carousel layout add assets image face update slick inner css

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,9 +49,56 @@ document.addEventListener("DOMContentLoaded", function () {
 // CAROUSEL
 $(document).ready(function () {
   $(".center").slick({
-    slidesToShow: 3,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
     dots: true,
     centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          // centerMode: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
   });
 });
 // CAROUSEL END
+
+// TESTI
+//Testimoni
+$(document).ready(function () {
+  $(".testi").slick({
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: "linear",
+  });
+});
